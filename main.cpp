e43ddae7ceafdebf8a9ea57fbf685d3677d74b1b
SimoneNaso <91719030+SimoneNaso@users.noreply.github.com>
2021-11-13 15:29:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, a=1, b=1, c=0;
+    cout << "Inserisci n" << endl;
+    cin >> n;
+    if (n>=2) {
+        cout << a << endl;
+        cout << b << endl;
+        for (int i=1; i<=(n-2); i++) {
+            c = a+b;
+            a=b;
+            b=c;
+            cout << c << endl;
+        }
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
+
